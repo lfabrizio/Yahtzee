edd9e3fc3b88a6c07f56e0d337ebbf49e9fc3d58
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-09-27 18:44:00 -0400

    yahtzee

--- src/com/fabrizio/Die.java
+++ src/com/fabrizio/Die.java
@@ -5,23 +5,23 @@ import java.util.Random;
 //v1.0 I have a die
 // v1.1 that die can be rolled to have a random value
 public class Die {
-    // get the die
-    //also store number of sides of the die
+    // get/create the die
     private int diceSides = 6; // dice sides
     private int diceValue = 0; //to store value of dice rolled
-    private int timesRolled = 0; // keep track of how many times rolled
 
-    public void Die(int sides) {
-        sides = diceSides;
-        diceValue = 0;
+    public Die() {
+        diceValue = 1;
+    }
+//overloading
+    public Die(int sides){
+        diceValue = 1;
+        this.diceValue = sides;
     }
 
-    public int roll() {
-        Random random = new Random(); //random
-
-        diceValue = random.nextInt(diceSides) + 1; // will give random number from zero to dicesides, so must add 1
-        return diceValue;
+    public void roll(Random rand){
+         diceValue = rand.nextInt(diceSides)+ 1;
     }
+
 
     public List<Die> Dice() {
         for (int i = 0; i < 6; i++) {
--- src/com/fabrizio/Yahtzee.java
+++ src/com/fabrizio/Yahtzee.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 import java.util.Random;
         import java.util.Scanner;
 
-//public class Yahtzee {
-//    int d1, d2, d3, d4, d5;
-//
-////    ArrayList<Integer> hand = new ArrayList<Integer>();
-//
-//        //user input
-//        Scanner dieAmt = new Scanner(System.in)
+        public class Yahtzee {
+          int d1, d2, d3, d4, d5;
+
+           ArrayList<Integer> hand = new ArrayList<Integer>();
+           Scanner dieAmt = new Scanner(System.in)
 
     };
